1ce463204f2b7727e46ee82592b42532f8cabc1c
Hüseyin YÜKSEL <elamedya@gmail.com>
2026-05-20 20:29:16 +0300

    Laptop uzerinden ilk temiz dagitim ve favicon guncellemesi

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,6 @@ import FloatingActions from './components/FloatingActions';
 export const metadata = {
   title: 'Ela Teknoloji | Web Tasarımı Yapan Firmalar',
   description: 'Kurumsal web tasarım, modern mühendislik çözümleri ve GEO altyapısı.',
-  icons: {
-    icon: '/favicon.ico',
 };
 
 export default function RootLayout({
